7a15c825be56848592e26c194506ee4ece33492c
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-07-03 12:40:09 -0700

    update model

--- model.py
+++ model.py
@@ -1,33 +1,73 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torch.autograd import Variable
 
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
+        resnet = models.resnet152(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
         
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
-        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        self.init_weights()
+        
+    def init_weights(self):
+        """Initialize the weights."""
+        self.linear.weight.data.normal_(0.0, 0.02)
+        self.linear.bias.data.fill_(0)
 
     def forward(self, images):
         features = self.resnet(images)
+        features = Variable(features.data)
         features = features.view(features.size(0), -1)
-        features = self.embed(features)
+        #features = self.embed(features)
+        features = self.bn(self.linear(features))
         return features
     
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        """Set the hyper-parameters and build the layers."""
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        #self.softmax = nn.Softmax()
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+    
+    def init_weights(self):
+        """Initialize weights."""
+        self.embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
     
     def forward(self, features, captions):
-        pass
+        """Decode image feature vectors and generates captions."""
+        embeddings = self.embed(captions[:, :-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1) 
+        lstm_out, self.hidden = self.lstm(embeddings)
+        outputs = self.linear(lstm_out)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states) # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))   # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)               # predicted: (batch_size)
+            sampled_ids.append(predicted)
+            if predicted == 1:
+                break
+            inputs = self.embed(predicted)              # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                # inputs: (batch_size, 1, embed_size)
+        
+        sampled_ids = torch.stack(sampled_ids, 1)       # sampled_ids: (batch_size, 20)
+        
+        return sampled_ids.squeeze().cpu().numpy().tolist()
